8e1488bb431a11787118597cc9e419d1d57d2441
mighty-s <sdm3285@naver.com>
2019-02-06 22:57:08 +0900

    공통 인터셉터 추가

--- src/java/com/common/intercepter/FrontInterceptor.java
+++ src/java/com/common/intercepter/FrontInterceptor.java
@@ -33,20 +33,38 @@ public class FrontInterceptor extends HandlerInterceptorAdapter {
 		
 		// [1] VO Setting
 		requestMap.forEach( (k, v) -> {
-			
 			if(v.length == 1) {
 				vo.put(k, v[0]);
 			
 			} else {
 				vo.put(k, v);
 			}
-			
 		});
 		
+		// [2] 필요정보 aBox객체에 저장함
+		//vo.put("remoteHost"  , req.getRemoteHost()); // Client의 Host
+		vo.put("remoteAddr"  , getIP(req)); // Client의 Host IP
+		vo.put("remoteURI"   , req.getRequestURI()); // Client가 요청한 URI
+		vo.put("remoteMethod", req.getMethod());     // Client가 요청한 Method (GET, POST, PUT, DELETE)
+		
 		// 세팅된 VO request 영역에 저장 후 Controller로 전송
-		req.setAttribute("initVO", vo);
+		req.setAttribute("defaultVO", vo);
 		return true;
 	}
 	
 	
+	/**
+	 * <pre> 
+	 *  사용자의 IP를 가져오는 메서드
+	 *  ( Reverse Proxy등 웹서버나 다른 곳을 거쳐올 경우 req.getRemoteAddr() 대신 사용)
+	 * </pre>
+	 * 
+	 * @author  Dong-Min Seol
+	 * @since   2019.02.03  
+	 */
+	public String getIP (HttpServletRequest req) {
+		String ip = req.getHeader("X-Forwarded-For");
+		
+		return ip;
+	}
 }
