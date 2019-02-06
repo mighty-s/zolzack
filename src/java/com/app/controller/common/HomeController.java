c18ced48218f71aff88929a6c47aa0c64f47541a
mighty-s <sdm3285@naver.com>
2019-02-06 22:55:21 +0900

    Main 컨트롤러 변경

--- src/java/com/app/controller/common/HomeController.java
+++ src/java/com/app/controller/common/HomeController.java
@@ -1,34 +1,40 @@
-package com.common.controller;
+package com.app.controller.common;
 
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.common.collection.VO;
+import com.common.controller.CommonController;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
-public class HomeController {
+public class HomeController extends CommonController {
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/index.do", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public ModelAndView home(@ModelAttribute("defaultVO") VO vo, Locale locale) {
+		ModelAndView mav = new ModelAndView("home");
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
 		
-		model.addAttribute("serverTime", formattedDate );
+		mav.addObject("serverTime", formattedDate);
+		mav.addObject("defaultVO", vo);
 		
-		return "home";
+		return mav;
 	}
 	
 }
